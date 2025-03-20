9565544676059c2c621de0cd5b604c0685462c82
joris chaintron <jorischaintron69@gmail.com>
2025-03-20 16:25:17 +0100

    Commit1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
-#include <Arduino.h>
+#include "Wire.h"
+#include "SSD1306.h"
 
-// put function declarations here:
-int myFunction(int, int);
+SSD1306Wire ecranOled(0x3c, 5, 4);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+   ecranOled.init();
+    
+    ecranOled.clear();
+
+    ecranOled.setFont(ArialMT_Plain_16);
+
+    ecranOled.drawString(0, 10, "Mon Appli v1.0");
+
+    ecranOled.display();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  
 }
